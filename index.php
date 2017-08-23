d6a5434a5ce54c63977d474270224f87f20f6f16
s3660898 <s3660898@student.rmit.edu.au>
2017-08-23 20:00:20 +1000

    more nonsense on showing page

--- index.php
+++ index.php
@@ -12,12 +12,10 @@
 		</header>
 		<nav>
 			<!--<p>This is the nav section.</p>-->
-			<a href="/~s3660898/wp/a2/showing.php">
-				<img src="/~s3660898/wp/a2/now_showing.png" alt="now showing">
-			</a>
+			<a href="/~s3660898/wp/a2/showing.php"><img src="/~s3660898/wp/a2/now_showing.png" alt="now showing"></a>
 		</nav>
 		<main>
-			<p>Nonsense on cinema renovations, features & so on...</p>
+			<p>Nonsense on cinema renovations, features &amp so on...</p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare gravida sem eu ultrices. Quisque vehicula, mi et posuere elementum, nunc dui lacinia neque, vitae volutpat ex ex eu nulla. Sed in vestibulum nibh. Quisque efficitur arcu tortor, ut sodales ligula bibendum et. Nulla nec dolor metus. Donec porta finibus risus, eu maximus sapien aliquam a. Cras magna nisl, commodo nec dui et, aliquam porttitor metus. Quisque eget lectus et arcu faucibus tristique. Proin sed nibh iaculis, blandit massa cursus, pharetra massa.</p>
 			<p>Integer dapibus auctor augue vitae laoreet. Aliquam felis nulla, porttitor sed leo eu, facilisis imperdiet tortor. Phasellus in nibh nec justo venenatis auctor ac egestas risus. Nulla quis posuere leo. Maecenas id risus eros. Etiam mauris tortor, aliquam nec magna quis, placerat dictum dolor. Cras eget magna odio. Nam efficitur iaculis nunc, et tincidunt sem tempor in. Nunc et augue velit. Donec luctus efficitur faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus eget scelerisque magna, ac tincidunt metus. Proin vel justo volutpat lorem consequat feugiat a sit amet enim.</p>
 			<p>Aenean et ullamcorper est. Proin sagittis sem ipsum, sed faucibus felis volutpat at. Praesent dictum fringilla lorem ut tristique. Nulla orci est, vulputate vel lorem ut, pretium faucibus justo. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar sapien commodo, fermentum nunc nec, gravida enim. Fusce ultrices consequat risus, vel egestas nulla placerat sed. Etiam et lectus eu turpis sagittis mattis. Vestibulum vitae congue felis, ac semper lectus.</p>
@@ -25,7 +23,7 @@
 			<p>Curabitur accumsan sapien eu eros auctor, sed fermentum arcu lacinia. Maecenas eu odio diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas pellentesque urna eget risus tristique, in mollis nisl iaculis. Fusce vel nibh gravida justo dignissim faucibus. Fusce nec blandit sapien. Phasellus elementum massa non posuere dignissim. Nam ac arcu ex. Proin tincidunt, est nec imperdiet volutpat, neque felis volutpat libero, non vulputate enim dolor in libero. Donec cursus tellus in euismod feugiat. Praesent eu tempor ex. Mauris sapien eros, ornare non lacinia auctor, faucibus lacinia diam. Maecenas dictum, metus non luctus lacinia, dolor dolor tempus quam, et pharetra diam tortor in libero. Praesent consectetur mollis lacus, sed consectetur sem consequat a. Ut vel venenatis mi, ac ornare metus.</p>
 		</main>
 		<footer>
-			<p>This is the footer.</p>
+			<p>Kieran Murray s3660898 &amp Will Cohen s3660470</p>
 		</footer>
 	</body>
 </html>
--- showing.php
+++ showing.php
@@ -7,16 +7,63 @@
 	
 	<body>
 		<header>
-			<h1>This is the header.</h1>
+			<img src="/~s3660898/wp/a2/logo.png" alt="silverado logo">
 		</header>
 		<nav>
-			<p>This is the nav section.</p>
+			<a href="/~s3660898/wp/a2/index.php"><img src="/~s3660898/wp/a2/home.png" alt="home"></a>
 		</nav>
 		<main>
-			<p>This webpage for 'Assignment 2' is under construction.</p>
+			<h1>NOW SHOWING:</h1>
+			<div class="image-gallery">
+				<img src="/~s3660898/wp/a2/movie_icons/AClockworkOrange.jpg" alt="A Clockwork Orange"/>
+				<img src="/~s3660898/wp/a2/movie_icons/ANewHope.jpg" alt="Star Wars: A New Hope"/>
+				<img src="/~s3660898/wp/a2/movie_icons/ApocalypseNow.jpg" alt="Apocalypse Now"/>
+				<img src="/~s3660898/wp/a2/movie_icons/GranTorino.jpg" alt="Gran Torino"/>
+				<img src="/~s3660898/wp/a2/movie_icons/IngloriousBasterds.jpg" alt="Inglorious Basterds"/>
+				<img src="/~s3660898/wp/a2/movie_icons/SevenSamurai.jpg" alt="Seven Samurai"/>
+				<img src="/~s3660898/wp/a2/movie_icons/WomanInTheDunes.jpg" alt="Woman In The Dunes"/>
+				<img src="/~s3660898/wp/a2/movie_icons/ThePrincessBride.jpg" alt="The Princess Bride"/>
+				<img src="/~s3660898/wp/a2/movie_icons/Up.jpg" alt="Up"/>
+				<img src="/~s3660898/wp/a2/movie_icons/LeonTheProfessional.jpg" alt="Leon The Professional">
+				<img src="/~s3660898/wp/a2/movie_icons/Casablanca.jpg" alt="Casablanca">
+				<img src="/~s3660898/wp/a2/movie_icons/TheThirdMan.jpg" alt="The Third Man">
+				<img src="/~s3660898/wp/a2/movie_icons/FiddlerOnTheRoof.jpg" alt="Fiddler On The Roof">
+				<img src="/~s3660898/wp/a2/movie_icons/DonnieDarko.jpg" alt="Donnie Darko">
+				<img src="/~s3660898/wp/a2/movie_icons/Brazil.jpg" alt="Brazil">
+				<img src="/~s3660898/wp/a2/movie_icons/PulpFiction.jpg" alt="Pulp Fiction">
+				<img src="/~s3660898/wp/a2/movie_icons/HowlsMovingCastle.jpg" alt="Howls Moving Castle">
+				<img src="/~s3660898/wp/a2/movie_icons/FearAndLoathingInLasVegas.jpg" alt="Fear And Loathing In Las Vegas">
+				<!--<img src="/~s3660898/wp/a2/movie_icons/.jpg" alt="">-->
+				<p style="clear: both;">
+			</div>
+		</main>
+		<main>
+			<form method='post'>
+				<fieldset><legend>Booking Form</legend>
+				  <p><label>Movie</label><select name=TBA></select></p>
+				  <p><label>Session</label><select name=TBA></select></p>
+				  <fieldset><legend>Seats</legend>
+					<fieldset><legend>Standard</legend>
+					  <p><label>Adult</label><select name=TBA></select></p>
+					  <p><label>Concession</label><select name=TBA></select></p>
+					  <p><label>Child</label><select name=TBA></select></p>
+					</fieldset>
+					<fieldset><legend>First Class</legend>
+					  <p><label>Adult</label><select name=TBA></select></p>
+					  <p><label>Child</label><select name=TBA></select></p>
+					</fieldset>
+					<fieldset><legend>Bean Bags</legend>
+					  <p><label>Adult</label><select name=TBA></select></p>
+					  <p><label>Family</label><select name=TBA></select></p>
+					  <p><label>Child</label><select name=TBA></select></p>
+					</fieldset>
+				  </fieldset>
+				  <p><button>Book</button></p>
+				</fieldset>
+			</form>
 		</main>
 		<footer>
-			<p>This is the footer.</p>
+			<p>Kieran Murray s3660898 &amp Will Cohen s3660470</p>
 		</footer>
 	</body>
 </html>
