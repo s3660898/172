4b321a60a4956d5b3f0949ba70cd6dc1ed1d44bf
s3660898 <s3660898@student.rmit.edu.au>
2017-08-23 17:29:32 +1000

    initial commit

---
+++ showing.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<head>
+	<title>A2 SHOWING Page</title>
+	<link type="text/css" rel="stylesheet" href="/~s3660898/wp/a2/wireframe.css"/>
+	</head>
+	
+	<body>
+		<header>
+			<h1>This is the header.</h1>
+		</header>
+		<nav>
+			<p>This is the nav section.</p>
+		</nav>
+		<main>
+			<p>This webpage for 'Assignment 2' is under construction.</p>
+		</main>
+		<footer>
+			<p>This is the footer.</p>
+		</footer>
+	</body>
+</html>
